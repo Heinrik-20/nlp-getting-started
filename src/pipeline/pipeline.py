cd5246133479ba6a6a12a7d1d7499e733101b7ce
Heinrik-20 <henryho3381@hotmail.com>
2024-08-17 19:00:12 +1000

    feat: implement sentence embedding

--- src/pipeline/pipeline.py
+++
@@ -1,6 +0,0 @@
-from strategy import EmbeddingStrategy
-from typing import List
-
-class Pipeline():
-    def __init__(self) -> None:
-        self.handlers: List[EmbeddingStrategy] = []
--- src/pipeline/sentence_embedding.py
+++ src/pipeline/sentence_embedding.py
@@ -1,13 +1,35 @@
+import pandas as pd
+
 from .strategy import EmbeddingStrategy
+from .cleaning import Cleaning
+from sentence_transformers import SentenceTransformer
 
 class SentenceEmbedding(EmbeddingStrategy):
-    def __init__(self, file_path: str) -> None:
+    def __init__(self, cleaning_cls: Cleaning) -> None:
+        
+        self.cleaning_cls = cleaning_cls
+        self.model = SentenceTransformer('sentence-transformers/all-mpnet-base-v2')
 
-        self.lda = None
-        self.tfidf = None
-
-        super(SentenceEmbedding, self).__init__(file_path)
+        super(SentenceEmbedding, self).__init__(cleaning_cls)
         return
     
     def generate_embedding(self):
-        return
+
+        print("Preparing data")
+        self.cleaning_cls()
+
+        print("Constructing sentences from documents")
+        train_corpus = [" ".join(doc) for doc in self.cleaning_cls.train_corpus]
+        test_corpus = [" ".join(doc) for doc in self.cleaning_cls.test_corpus]
+
+        print("Encoding sentences")
+        train_embeddings = self.model.encode(train_corpus)
+        test_embeddings = self.model.encode(test_corpus)
+
+        print("Preparing dataframe")
+        columns = [f"mpnet_embedding_{i}" for i in range(len(train_embeddings[0]))]
+        train_df = pd.DataFrame(data=train_embeddings, columns=columns)
+        test_df = pd.DataFrame(data=test_embeddings, columns=columns)
+        train_df['target'] = self.cleaning_cls.train_df['target']
+
+        return train_df, test_df
